cd121146a9432c06a03c2a0ae681ebdbafbc3e0a
Mahmoud Nasser <nassermanone@gmail.com>
2023-03-13 15:09:46 +0200

    First Database Sqlite3 App

---
+++ 14-FirstDBApp/app1/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class PersonName(models.Model):
+    FirstName = models.CharField(max_length=20)
+    LastName = models.CharField(max_length=20)
+    
+    def __str__(self) :
+        return self.FirstName + ' ' + self.LastName
+    
+    
+    
